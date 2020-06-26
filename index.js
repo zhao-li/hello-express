3a67ce5764883a21958c0136d4daf60311723399
Zhao Li <zhao.li@startingspark.com>
2020-06-26 08:55:54 -1000

    organize greetings routes to another folder

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const greetings = require('./greetings/routes');
+
 app.use(express.json());
 
-app.get('/greetings/', (req, res) => {
-  res.send("greetings, world");
 });
+
+app.use('/greetings/', greetings);
 
 app.listen(port, () => console.log(
   `hello-express listening at http://localhost:${port}`)
